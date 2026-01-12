9b2c5c1220643ac2825a18761eb422462efad543
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-01-12 10:34:20 +0545

    Updated method of operations in a singly linked list.

--- linked-list/LinkedList.c
+++ linked-list/LinkedList.c
@@ -44,6 +44,58 @@ void insertNode(Node **head) {
 	printf("Node inserted\n");
 }
 
+// Function to insert a head node
+void insertHeadNode(Node **head) {
+  int value;
+	printf("Enter value to insert : ");
+	scanf("%d", &value);
+
+	Node *newNode = (Node *) malloc(sizeof(Node));
+	newNode->data = value;
+	newNode->next = NULL;
+
+	if(*head == NULL) {
+		*head = newNode;
+		printf("Head node created\n");
+		return;
+	}
+	Node *temp = *head;
+	newNode->next = temp;
+	*head= newNode;
+	printf("Node inserted successfully!\n");
+}
+
+// Function to add a node at specific index
+void insertNodeAtIndex(Node **head) {
+  traverseNode(*head);
+  int index, i, data;
+  printf("Enter the index to insert at : ");
+  scanf("%d", &index);
+  printf("Enter the data to insert : ");
+  scanf("%d", &data);
+
+  Node *newNode = (Node *) malloc(sizeof(Node));
+  if (newNode == NULL) {
+    printf("Memory allocation failed.\n");
+    return;
+  }
+  newNode->data = data;
+  newNode->next = NULL;
+
+  Node *temp = *head;
+  for(i=0; i<index-1 && temp != NULL; i++) {
+    temp = temp->next;
+  }
+  if(temp == NULL) {
+    printf("Invalid index.\n");
+    free(newNode);
+    return;
+  }
+  newNode->next = temp->next;
+  temp->next = newNode;
+  printf("Node inserted at index %d successfully!\n", index);
+}
+
 // function to search node element position
 void searchNode(Node *head) {
 	int value;
@@ -63,38 +115,35 @@ void searchNode(Node *head) {
 	printf("The value %d not found in the list.\n", value);
 }
 
-// Function to delete a specific node
+// Function to delete the last tail node
 void deleteNode(Node **head) {
 	if(*head == NULL) {
 		printf("The list is empth. Nothing can be deleted.\n");
 		return;
 	}
-	int value;
-	printf("Enter value to delete : ");
-	scanf("%d", &value);
-
 	Node *temp = *head;
 	Node *prev = NULL;
-	// If head node is to be deleted
-	if(temp != NULL && temp->data == value) {
-		*head = temp->next;
-		free(temp);
-		printf("Node deleted.\n");
-		return;
-	}
-	// Loop to search the node
-	while(temp != NULL && temp->data != value) {
+
+	while(temp->next != NULL) {
 		prev = temp;
 		temp = temp->next;
 	}
 
-	if(temp == NULL) {
-		printf("The required value not found in the list.\n");
-		return;
-	}
-	prev->next = temp->next;
+	prev->next = NULL;
 	free(temp);
 	printf("Node deleted.\n");
+}
+
+// Function to delete head node
+void deleteHeadNode(Node **head) {
+  if(*head == NULL) {
+    printf("Node is empty can't delete.\n");
+    return;
+  }
+  Node *temp = *head;
+  *head = temp->next;
+  free(temp);
+  printf("Head node deleted successfully!\n");
 }
 
 // Function to release heap memory
@@ -113,11 +162,13 @@ int main() {
 	Node *head = NULL;
 
 	do {
-		printf("1. Insert Node\n");
-		printf("2. Display Nodes\n");
-		printf("3. Delete Node\n");
-		printf("4. Search Node\n");
-		printf("5. Exit\n");
+		printf("1. Insert Node (At end)\n");
+		printf("2. Insert Node (At Head)\n");
+		printf("3. Display Nodes\n");
+		printf("4. Delete Node (At end)\n");
+		printf("5. Delete Node (At first)\n");
+		printf("6. Insert Node (via index)\n");
+		printf("7. Exit\n");
 		printf("Enter your choice : ");
 		scanf("%d", &choice);
 		switch(choice) {
@@ -125,21 +176,27 @@ int main() {
 				insertNode(&head);
 				break;
 			case 2:
-				traverseNode(head);
+			  insertHeadNode(&head);
 				break;
 			case 3:
-				deleteNode(&head);
+				traverseNode(head);
 				break;
 			case 4:
-				searchNode(head);
+				deleteNode(&head);
 				break;
 			case 5:
-				printf("Exiting program...");
+        deleteHeadNode(&head);
+    		break;
+      case 6:
+        insertNodeAtIndex(&head);
+        break;
+			case 7:
+				printf("Exiting program...\n");
 				break;
 			default:
 				printf("Invalid choice\n");
 		}
-	} while (choice != 5);
+	} while (choice != 7);
 	freeNodes(head);
 	return 0;
 }
