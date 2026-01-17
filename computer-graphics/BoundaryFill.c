d37a0f2d24b45fc0e8581fd08a7ce9d4cf1d3136
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-01-17 17:50:17 +0545

    Updated the code package from SDL/SDL2 to the graphics.h

--- computer-graphics/BoundaryFill.c
+++ computer-graphics/BoundaryFill.c
@@ -1,35 +1,13 @@
-#include <SDL2/SDL.h>
-#include <stdbool.h>
+#include <graphics.h>
 #include <stdio.h>
 
-#define WIDTH 800
-#define HEIGHT 600
-
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-SDL_Surface *canvas = NULL;
-
-Uint32 getPixel(int x, int y) {
-  Uint32 *pixels = (Uint32 *)canvas->pixels;
-  return pixels[y * WIDTH + x];
-}
-
-void setPixel(int x, int y, Uint32 color) {
-  Uint32 *pixels = (Uint32 *)canvas->pixels;
-  pixels[y * WIDTH + x] = color;
-}
-
-void boundaryFill(int x, int y, Uint32 boundaryColor, Uint32 fillColor)
-{
-  if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-    return;
-
-  Uint32 currentColor = getPixel(x, y);
+void boundaryFill(int x, int y, int boundaryColor, int fillColor) {
+  int currentColor = getpixel(x, y);
 
   if (currentColor == boundaryColor || currentColor == fillColor)
     return;
 
-  setPixel(x, y, fillColor);
+  putpixel(x, y, fillColor);
 
   boundaryFill(x + 1, y, boundaryColor, fillColor);
   boundaryFill(x - 1, y, boundaryColor, fillColor);
@@ -37,51 +15,20 @@ void boundaryFill(int x, int y, Uint32 boundaryColor, Uint32 fillColor)
   boundaryFill(x, y - 1, boundaryColor, fillColor);
 }
 
-int main()
-{
-  SDL_Init(SDL_INIT_VIDEO);
+int main() {
+  int gd = DETECT, gm;
 
-  window = SDL_CreateWindow("Boundary Fill - Sushank Ghimire", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+  initgraph(&gd, &gm, "");
 
-  renderer = SDL_CreateRenderer(window, -1, 0);
+  setbkcolor(WHITE);
 
-  canvas = SDL_CreateRGBSurfaceWithFormat(0, WIDTH, HEIGHT, 32, SDL_PIXELFORMAT_RGBA8888);
+  setcolor(BLACK);
+  rectangle(200, 150, 600, 450);
 
-  /* White background */
-  Uint32 white = SDL_MapRGB(canvas->format, 255, 255, 255);
-  SDL_FillRect(canvas, NULL, white);
-
-  /* Black boundary rectangle */
-  Uint32 black = SDL_MapRGB(canvas->format, 0, 0, 0);
-
-  SDL_Rect top    = {200, 150, 400, 2};
-  SDL_Rect bottom = {200, 448, 400, 2};
-  SDL_Rect left   = {200, 150, 2, 300};
-  SDL_Rect right  = {598, 150, 2, 300};
-
-  SDL_FillRect(canvas, &top, black);
-  SDL_FillRect(canvas, &bottom, black);
-  SDL_FillRect(canvas, &left, black);
-  SDL_FillRect(canvas, &right, black);
-
-  /* Boundary fill inside rectangle */
-  Uint32 blue = SDL_MapRGB(canvas->format, 0, 0, 255);
-  boundaryFill(300, 300, black, blue);
-
-  /* Render to screen */
-  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, canvas);
-  SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
-  SDL_RenderPresent(renderer);
+  boundaryFill(300, 300, BLACK, BLUE);
 
   getchar();
-
-  /* Screen Cleanup */
-  SDL_DestroyTexture(texture);
-  SDL_FreeSurface(canvas);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  closegraph();
 
   return 0;
 }
