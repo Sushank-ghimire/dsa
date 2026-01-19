304a2e090ac0e4f0d851201fd3965727f007a74a
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-01-19 18:26:16 +0545

    Implemented functionality of heap sort.

--- sorting/Sorting.c
+++ sorting/Sorting.c
@@ -4,6 +4,8 @@
 void swap(int *arr, int i, int j);
 void bubbleSort(int arr[], int size);
 void selectionSort(int arr[], int size);
+void heapSort(int arr[], int size);
+void heapify(int arr[], int size, int i);
 
 int main() {
   int choice;
@@ -34,6 +36,9 @@ int main() {
         break;
       case 2:
         selectionSort(arr, size);
+        break;
+      case 3:
+        heapSort(arr, size);
         break;
       case 7:
         printf("Exiting program...\n");
@@ -79,5 +84,45 @@ void selectionSort(int arr[], int size) {
         min_index = j;
     }
     swap(arr, min_index, i);
+  }
+}
+
+void heapify(int arr[], int n, int i) {
+  int largest = i;
+  // left index
+  int l = 2 * i + 1;
+  // right index
+  int r = 2 * i + 2;
+
+  // if left child is larger than root
+  if(l < n && arr[l] > arr[largest])
+    largest = l;
+
+  // if right child is larger than largest so far
+  if(r < n && arr[r] > arr[largest])
+    largest = r;
+
+  // if largest is not root
+  if(largest != i) {
+    swap(arr, i, largest);
+    heapify(arr, n, largest);
+  }
+}
+
+void heapSort(int arr[], int size) {
+  int i, j;
+  // Heap sort algorithm
+  for (i = size / 2 - 1; i >= 0; i--)
+    heapify(arr, size, i);
+
+  // One by one extract an element from heap
+  for (i = size - 1; i > 0; i--) {
+    // Move current root to end
+    int temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+
+    // Call max heapify on the reduced heap
+    heapify(arr, i, 0);
   }
 }
