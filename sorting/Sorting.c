f90b072254e7e3f88a16e7e3d5783a9282471304
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-02-02 18:28:37 +0545

    Added program implementation for the merge sort.

--- sorting/Sorting.c
+++ sorting/Sorting.c
@@ -2,12 +2,24 @@
 #define MAX 50
 
 void swap(int *arr, int i, int j);
+
+// Bubble sort function declarations
 void bubbleSort(int arr[], int size);
+
+// Selection sort function declarations
 void selectionSort(int arr[], int size);
+
+// Heap sort function declarations
 void heapSort(int arr[], int size);
 void heapify(int arr[], int size, int i);
+
+// Quick sort function declarations
 int partition(int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
+
+// Merge sort function declarations
+void merge(int arr[], int low, int mid, int high);
+void mergeSort(int arr[], int low, int high);
 
 int main() {
   int choice;
@@ -41,6 +53,9 @@ int main() {
         break;
       case 3:
         heapSort(arr, size);
+        break;
+      case 4:
+        mergeSort(arr, 0, size);
         break;
       case 5:
         quickSort(arr, 0, size - 1);;
@@ -151,5 +166,47 @@ void quickSort(int arr[], int low, int high) {
     int partitionIndex = partition(arr, low, high);
     quickSort(arr, low, partitionIndex - 1);
     quickSort(arr, partitionIndex + 1, high);
+  }
+}
+
+void merge(int arr[], int low, int mid, int high) {
+  int i, j, k;
+  int n1 = mid - low + 1;
+  int n2 = high - mid;
+  int leftArr[n1], rightArr[n2];
+
+  for(i=0; i<n1; i++)
+    leftArr[i] = arr[low+1];
+  for(i=0; i<n2; i++)
+    rightArr[i] = arr[mid + 1 + j];
+
+  i = 0, j = 0, k = low;
+  while(i < n1 && j < n2) {
+    if(leftArr[i] <= rightArr[j]) {
+      arr[k] = leftArr[i];
+      i++;
+    } else {
+      arr[k] = rightArr[j];
+      j++;
+    }
+    k++;
+  }
+  while(i < n1) {
+    arr[k] = leftArr[i];
+    i++; k++;
+  }
+  while(j < n2) {
+    arr[k] = rightArr[j];
+    j++; k++;
+  }
+}
+
+void mergeSort(int arr[], int low, int high) {
+  if(low < high) {
+    int mid = low + (high - low) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
   }
 }
