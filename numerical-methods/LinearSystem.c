afe0260cc5b00a98e11e5183eee26f6c6b4baaf7
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-02-16 20:18:34 +0545

    Added program for the implementation of the LU factorization.

--- numerical-methods/LinearSystem.c
+++ numerical-methods/LinearSystem.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 
 #define MAX 50
+#define EPSILON 1e-6
 
 int input(const char *msg);
 
@@ -49,3 +51,98 @@ int input(const char *msg) {
   scanf("%d", &data);
   return data;
 }
+
+// Do Little LU
+/*
+ * Input: Enter the order of the square matrix: 3
+ *        2 -1 -2
+ *       -4  6  3
+ *       -4 -2  8
+ * Lower Triangular Matrix (L):
+ *        1.00  0.00  0.00
+ *       -2.00  1.00  0.00
+ *       -2.00 -1.00  1.00
+ * Upper Triangular Matrix (U):
+ *        2.00 -1.00 -2.00
+ *        0.00  4.00 -1.00
+ *        0.00  0.00  3.00
+ */
+void doLittleLU() {
+  int n;
+  float A[MAX][MAX];
+  float L[MAX][MAX];
+  float U[MAX][MAX];
+
+  n = input("Enter the order of the square matrix : ");
+  printf("Enter the matrix elements (row-wise):\n");
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      printf("A[%d][%d]: ", i+1, j+1);
+      scanf("%f", &A[i][j]);
+    }
+  }
+
+  /* Initialize L and U to zero */
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      L[i][j] = 0;
+      U[i][j] = 0;
+    }
+  }
+
+  /* Do-Little LU Decomposition */
+  for(int i = 0; i < n; i++) {
+    /* Compute U matrix */
+    for(int k = i; k < n; k++) {
+      float sum = 0;
+      for(int j = 0; j < i; j++)
+        sum += L[i][j] * U[j][k];
+
+      U[i][k] = A[i][k] - sum;
+    }
+
+    /* Check for zero pivot */
+    if(fabs(U[i][i]) < EPSILON) {
+      printf("Mathematical Error: Division by zero.\n");
+      return;
+    }
+
+    /* Compute L matrix */
+    for(int k = i; k < n; k++) {
+      if(i == k)
+        L[i][i] = 1;  // Diagonal = 1
+      else {
+        float sum = 0;
+        for(int j = 0; j < i; j++)
+          sum += L[k][j] * U[j][i];
+
+        L[k][i] = (A[k][i] - sum) / U[i][i];
+      }
+    }
+    }
+
+  /* Display L matrix */
+  printf("\nLower Triangular Matrix (L):\n");
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++)
+      printf("%8.2f ", L[i][j]);
+    printf("\n");
+  }
+
+  /* Display U matrix */
+  printf("\nUpper Triangular Matrix (U):\n");
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++)
+      printf("%8.2f ", U[i][j]);
+    printf("\n");
+  }
+}
+
+// Cholesky Method
+void choleskyMethod(){}
+
+// Jacobi Method
+void jacobiMethod(){}
+
+// Gauss Seidel Method
+void gaussSeidelMethod(){}
