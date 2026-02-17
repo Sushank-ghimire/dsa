936b6e60a62b985dcec6223ca07d27ec3137827b
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-02-17 19:59:22 +0545

    Added program for the composite simpson's one third and it's one third method.

--- numerical-methods/SimpsonMethods.c
+++ numerical-methods/SimpsonMethods.c
@@ -7,6 +7,8 @@ double func(double x);
 // Simpson's method functions
 void simpsonsOneThird();
 void compositeSimpsonsOneThird();
+void simpsonsThreeEighth();
+void compositeSimpsonsThreeEighth();
 
 int main() {
   int choice;
@@ -14,7 +16,7 @@ int main() {
     printf("1. Simpson's 1/3\n");
     printf("2. Composite Simpson's 1/3\n");
     printf("3. Simpson's 3/8\n");
-    printf("4. Composite Simpson's3/8\n");
+    printf("4. Composite Simpson's 3/8\n");
     printf("5. Exit\n");
     choice = input("Enter your choice : ");
 
@@ -24,6 +26,12 @@ int main() {
         break;
       case 2:
         compositeSimpsonsOneThird();
+        break;
+      case 3:
+        simpsonsThreeEighth();
+        break;
+      case 4:
+        compositeSimpsonsThreeEighth();
         break;
       case 5:
         printf("Exiting program...\n");
@@ -93,4 +101,53 @@ void compositeSimpsonsOneThird() {
 
   double result = (h / 3.0) * sum;
   printf("Approximate integral using Composite Simpson's 1/3 rule: %.2lf\n", result);
+}
+
+/*
+ * Formula: ∫[a to b] f(x) dx ≈ (3h/8) * [f(a) + 3f(a+h) + 3f(a+2h) + f(b)]
+ * where h = (b-a)/3
+ */
+void simpsonsThreeEighth() {
+  double a = input("Enter lower limit a: ");
+  double b = input("Enter upper limit b: ");
+
+  double h = (b - a) / 3.0;
+  double result = (3 * h / 8.0) * (func(a) + 3 * func(a + h) + 3 * func(a + 2 * h) + func(b));
+
+  printf("Approximate integral using Simpson's 3/8 rule: %.2lf\n", result);
+}
+
+/*
+ * ∫[a to b] f(x) dx ≈ (3h/8) * [f(x0) + 3(f(x1)+f(x2)+f(x4)+f(x5)+...) + 2(f(x3)+f(x6)+...) + f(xn)]
+ * where n is multiple of 3, h = (b-a)/n, xi = a + i*h
+ */
+ void compositeSimpsonsThreeEighth() {
+  double a, b;
+  int n;
+
+  printf("Enter lower limit a: ");
+  scanf("%lf", &a);
+  printf("Enter upper limit b: ");
+  scanf("%lf", &b);
+  printf("Enter subintervals n (must be multiple of 3): ");
+  scanf("%d", &n);
+
+  if (n % 3 != 0) {
+    printf("Error: n must be a multiple of 3.\n");
+    return;
+  }
+
+  double h = (b - a) / n;
+  double sum = func(a) + func(b);
+
+  for (int i = 1; i < n; i++) {
+    double x = a + i * h;
+    if (i % 3 == 0)
+      sum += 2 * func(x); // multiples of 3
+    else
+      sum += 3 * func(x); // others
+  }
+
+  double result = (3 * h / 8.0) * sum;
+  printf("Approximate integral using Composite Simpson's 3/8 rule: %.2lf\n", result);
 }
