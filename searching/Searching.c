5a7a2ac8157a0d84f91f1bd680e8449319027187
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-02-19 12:06:57 +0545

    Fixed code of the searching algorithms.

--- searching/Searching.c
+++ searching/Searching.c
@@ -15,43 +15,41 @@ int main() {
     printf("Enter your choice : ");
     scanf("%d", &choice);
     int arr[MAX];
-    switch (choice) {
-      case 1:
-      case 2:
-        printf("Enter the size of array : ");
-        scanf("%d", &size);
-        printf("Enter the elements of the array : \n");
-        for(int i=0; i<size; i++) {
-          printf("Element for index[%d] : ", i);
-          scanf("%d", &arr[i]);
-        }
-        if(choice == 1){
-          bubbleSort(arr, size);
-          printf("The sorted array is : \n");
-          for(int i=0; i<size; i++)
-            printf("Element of index[%d] is %d.\n", i, arr[i]);
-          printf("\n");
-          int search, target;
-          while(search) {
-            printf("Enter element to search in array : ");
-            scanf("%d", &target);
-            int index = binarySearch(arr, 0, size-1, target);
-            if(index == -1) {
-              printf("Element %d not found in provided array.\n", target);
-            } else {
-              printf("Element %d found at index %d.\n", target, index);
-            }
-            printf("Enter your choice (0 to exit) : ");
-            scanf("%d", &search);
+
+    if(choice < 3 && choice >= 1) {
+      printf("Enter size of array : ");
+      scanf("%d", &size);
+      printf("Enter elements of the array : \n");
+      for(int i=0; i<size; i++) {
+        printf("Element for index[%d] : ", i);
+        scanf("%d", &arr[i]);
+      }
+
+      if(choice == 1){
+        bubbleSort(arr, size);
+        printf("The sorted array is : \n");
+        for(int i=0; i<size; i++)
+          printf("Element of index[%d] is %d.\n", i, arr[i]);
+        printf("\n");
+        int target = 1;
+        while(target) {
+          printf("Enter your choice (0 to exit || search target) : ");
+          scanf("%d", &target);
+          if(!target) break;
+          int index = binarySearch(arr, 0, size-1, target);
+          if(index == -1) {
+            printf("Element %d not found.\n", target);
+          } else {
+            printf("Element %d found at index %d.\n", target, index);
           }
-        } else if (choice == 2)
-          sequentialSearch(arr, size);;
-        break;
-      case 3:
-        printf("Exiting program...\n");
-        break;
-      default:
-        printf("Invalid choice!\n");
+        }
+      } else if (choice == 2)
+        sequentialSearch(arr, size);
+    } else if (choice == 3) {
+      printf("Exiting program...\n");
+      break;
+    } else {
+      printf("Invalid choice!\n");
     }
   } while (choice != 3);
   return 0;
@@ -59,9 +57,9 @@ int main() {
 
 
 void sequentialSearch(int arr[MAX], int size) {
-  int target, index = -1, choice;
+  int target, index = -1;
   do {
-    printf("Enter element to search in array : ");
+    printf("Enter your choice (0 to exit || search target) : ");
     scanf("%d", &target);
 
     for(int i=0; i<size; i++) {
@@ -71,13 +69,11 @@ void sequentialSearch(int arr[MAX], int size) {
       }
     }
     if(index == -1) {
-      printf("Element %d not found in provided array.\n", target);
+      printf("Element %d not found.\n", target);
     } else {
       printf("Element %d found at index %d.\n", target, index);
     }
-    printf("Enter your choice (0 to exit) : ");
-    scanf("%d", &choice);
-  } while (choice != 0);
+  } while (target != 0);
 }
 
 void swap(int *arr, int i, int j) {
@@ -98,16 +94,13 @@ void bubbleSort(int arr[], int size) {
 }
 
 int binarySearch(int arr[], int low, int high, int target) {
-  int mid = (low + high) / 2;
-  if(high >= low) {
+  if (high >= low) {
     int mid = (low + high) / 2;
     if (arr[mid] == target)
       return mid;
-
-    if(arr[mid] > target)
+    if (arr[mid] > target)
       return binarySearch(arr, low, mid - 1, target);
-
-    return binarySearch(arr, low, mid - 1, target);
+    return binarySearch(arr, mid + 1, high, target);
   }
   return -1;
 }
